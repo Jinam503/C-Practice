c854544310ebba3dda6cdf1ca76338d1a03eb7e1
Jinam503 <jinam1234567@gmail.com>
2022-12-04 23:46:38 +0900

    221204

--- Tree.c
+++ Tree.c
@@ -9,13 +9,20 @@ void Print(int line){
         for(int j = 1; j <= i; j++){
             if(j == 1) for(int k = 0; k < (line - i) * 2; k++) printf(" ");
             cnt < 10 ? printf("%d   ", cnt++) : printf("%d  ", cnt++);
-            
         }
         printf("\n");
         for(int j = 0; j < (line - i) * 2 - 1; j++) printf(" ");
-        if(i != line) for(int j = 1; j <= i; j++) printf("/ \\ ");
-        printf("\n");
+        if(i != line) {
+            for(int j = 1; j <= i; j++) printf("/ \\ ");
+            printf("\n");
+        }
     }
+    for(int i = 0; i < 1; i++){
+        for(int k = 0; k < (line - 1) * 2 - 2; k++) printf(" ");
+        printf("|   |\n");
+    }
+    for(int k = 0; k < (line - 1) * 2 - 2; k++) printf(" ");
+    printf("-----\n");
 }
 
 int main() {
