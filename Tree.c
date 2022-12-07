1997f2c56ad77b10e7927bb8cbaaeb212c5b8ebf
Jinam503 <jinam1234567@gmail.com>
2022-12-07 15:34:58 +0900

    221207

--- Tree.c
+++ Tree.c
@@ -1,12 +1,68 @@
-// Online C compiler to run C program online
-#include <stdio.h>
-#include <windows.h>
 
+#include <stdio.h>
+typedef struct{
+    struct ChristmasTree *left, *right;
+    int data;
+} ChristmasTree;
+
+void Print(int line);ChristmasTree* CreateTree(int line);ChristmasTree* MakeNode(int data);
+int line, count;
+int main() {
+    
+	scanf("%d", &line);
+	ChristmasTree* root = CreateTree(line);
+	printf("%d", root->data);
+	Print(line);
+	
+
+	return 0;
+}
+ChristmasTree* MakeNode(int data){
+    ChristmasTree* nN = (ChristmasTree*)malloc(sizeof(ChristmasTree));
+	nN->data = data;
+	nN->left = NULL;
+	nN->right = NULL;
+    return nN;
+}
+ChristmasTree* CreateTree(int line){
+  ChristmasTree* root = (ChristmasTree*)malloc(sizeof(ChristmasTree));
+	root->data = 1;
+  int count = 1;
+	ChristmasTree* list[15] = {0};
+	list[line-1] = root;
+	for(int i = 1; i < line; i++){
+		for(int j = 0; j < i; j ++){
+			if( j == 0){
+				ChristmasTree* lN = MakeNode(++count);
+				ChristmasTree* rN = MakeNode(++count);
+				list[line-1]->left = lN;
+				list[line-1]->right = rN;
+				list[line-1] = lN;
+				list[i-1] = rN;
+			}
+			else{
+				ChristmasTree* rN = MakeNode(++count);
+				list[i - (j+1)]->left = list[i-j];
+				list[i - (j+1)]->right = rN;
+				list[i - (j+1)] = rN;
+			}
+		}
+	}
+	
+	return root;
+}
+//ChristmasTree* Find(ChristmasTree* root,int keyNum){
+	
+//}
 void Print(int line){
 
     printf("\nChristmas Tree!\n");
+		if(line == 1){
+			printf("  â˜†\n  1\n  |");
+				return;
+		}
     for(int k = 0; k < (line - 1) * 2 ; k++) printf(" ");
-    printf("¡Ù\n");
+    printf("â˜†\n");
     int cnt = 1;
     for(int i = 1; i <= line; i++){
         for(int j = 1; j <= i; j++){
@@ -28,12 +84,3 @@ void Print(int line){
     printf("_____\n");
 }
 
-
-int main() {
-    // Write C code here
-    int line;
-    scanf("%d", &line);
-    Print(line);
-
-    return 0;
-}
